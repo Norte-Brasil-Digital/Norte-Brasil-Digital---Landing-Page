eaa91566ad0ea38d7aa7b2158685eae36987baa1
Fábio Vinícius Lima <limavfabio@gmail.com>
2024-09-30 15:27:25 -0300

    Send email using formspree
    On form of contact section

--- src/components/index/ContactSection.tsx
+++ src/components/index/ContactSection.tsx
@@ -25,7 +25,10 @@ export default function ContactSection() {
           Converse com nosso time especializado, vamos encontrar a solução ideal para o seu negócio.
         </p>
       </div>
-      <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
+      <form
+        action="https://formspree.io/f/xqazrgyw"
+        method="POST"
+        className="mx-auto mt-16 max-w-xl sm:mt-20">
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
           <div>
             <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
@@ -79,6 +82,7 @@ export default function ContactSection() {
                 name="email"
                 type="email"
                 autoComplete="email"
+                required
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
@@ -108,6 +112,7 @@ export default function ContactSection() {
                 id="message"
                 name="message"
                 rows={4}
+                required
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 defaultValue={''}
               />
